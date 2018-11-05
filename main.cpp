4eb7d9accaa4c5a6a232ce76fe9a74345e800fe1
Roman Grewal <roman@debian>
2018-11-04 19:00:54 -0500

    updated

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
-#include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <string>
+#include <string.h>
 #include <iostream>
 #include <map>
 #include <vector>
+#include <system_error>
+#include <execinfo.h>
+#include <sstream>
+#include <iterator>
 
 std::string process(std::string const& s) {
   std::string::size_type pos = s.find(' ');
@@ -26,6 +29,26 @@ enum requestMethods {
   DELETE  
 };
 
+class system_error_stacktrace : public std::system_error {
+  using std::system_error::system_error;
+  public:
+  std::string stacktrace() const throw() {
+    void * array[1000];
+    int nSize = backtrace(array, 1000);
+    char** symbols = backtrace_symbols(array, nSize);
+
+    std::vector<std::string> v(symbols, symbols + nSize);
+
+    const char* const delim = "\n";
+
+    std::ostringstream imploded;
+    std::copy(v.begin(), v.end(),
+                   std::ostream_iterator<std::string>(imploded, delim));
+    free(symbols);
+    return imploded.str();
+  }
+};
+
 int main () {
   int server_fd; 
   int new_socket; 
@@ -41,61 +64,88 @@ int main () {
   requestsMethodsMap["PATCH"] = PATCH;
   requestsMethodsMap["DELETE"] = DELETE;
 
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    perror("Error in socket");
-    return 0;
-  }
+  std::string responseHeader = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
+  std::string responseHeaderNotAllowed = "HTTP/1.1 405 Method Not Allowed\nContent-Type: text/plain\nContent-Length: ";
 
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
-
-  memset(address.sin_zero, '\0', sizeof(address.sin_zero));
-
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("Error in bind");
-    return 0;
-  }
-
-  if (listen(server_fd, 10) < 0) {
-    perror("Error in listen");
-    return 0;
-  }
-
-  while (1) {
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-      perror("Error in accept");
-      return 0;
+  try {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+      throw system_error_stacktrace(std::error_code(server_fd, std::generic_category()), "Error in socket");
     }
 
-    std::vector<char> buffer(30000);
-    valread = read(new_socket , &buffer[0], buffer.size());
-    std::string str(buffer.begin(), buffer.end());
-    std::cout << str << std::endl;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    std::string method = process(str);
-    std::string result;
-    switch (requestsMethodsMap[method]) {
-      case GET: 
-        result = "YOU SENT A GET REQUEST\n";
-        break;
-      case POST:
-        result = "YOU SENT A POST REQUEST\n";
-        break;
-      case PUT:
-        result = "YOU SENT A PUT REQUEST\n";
-        break;
-      case PATCH:
-        result = "YOU SENT A PATCH REQUEST\n";
-        break;
-      case DELETE:
-        result = "YOU SENT A DELETE REQUEST\n";
-        break;
+    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+    int errno;
+    if (errno = bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+      throw system_error_stacktrace(std::error_code(errno, std::generic_category()), "Bind failed");
+    }
+    if (errno = listen(server_fd, 10) < 0) {
+      throw system_error_stacktrace(std::error_code(errno, std::generic_category()), "Error in listen");
     }
 
-    write(new_socket, result.c_str(), result.length());
+    while (true) {
+      if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        throw system_error_stacktrace(std::error_code(new_socket, std::generic_category()), "Error in accept");
+      }
 
-    close(new_socket);
+      std::vector<char> buffer(30000);
+      valread = read(new_socket , &buffer[0], buffer.size());
+      std::string str(buffer.begin(), buffer.end());
+      std::cout << str << std::endl;
+
+      std::string method = process(str);
+      std::string result;
+      std::string body;
+      int contentLength;
+      std::string contentLengthStr;
+      switch (requestsMethodsMap.find(method)->second) {
+        case GET: 
+          body = "YOU SENT A GET REQUEST\n";
+          contentLength = body.length();
+          contentLengthStr = std::to_string(contentLength);
+          result = responseHeader + contentLengthStr + "\n\n" + body;
+          break;
+        case POST:
+          body = "YOU SENT A POST REQUEST\n";
+          contentLength = body.length();
+          contentLengthStr = std::to_string(contentLength);
+          result = responseHeader + contentLengthStr + "\n\n" + body;
+          break;
+        case PUT:
+          body = "YOU SENT A PUT REQUEST\n";
+          contentLength = body.length();
+          contentLengthStr = std::to_string(contentLength);
+          result = responseHeader + contentLengthStr + "\n\n" + body;
+          break;
+        case PATCH:
+          body = "YOU SENT A PATCH REQUEST\n";
+          contentLength = body.length();
+          contentLengthStr = std::to_string(contentLength);
+          result = responseHeader + contentLengthStr + "\n\n" + body;
+          break;
+        case DELETE:
+          body = "YOU SENT A DELETE REQUEST\n";
+          contentLength = body.length();
+          contentLengthStr = std::to_string(contentLength);
+          result = responseHeader + contentLengthStr + "\n\n" + body;
+          break;
+        default:
+          body = "METHOD NOT ALLOWED\n";
+          contentLength = body.length();
+          contentLengthStr = std::to_string(contentLength);
+          result = responseHeaderNotAllowed + contentLengthStr + "\n\n" + body;
+          break;
+      }
+
+      write(new_socket, result.c_str(), result.length());
+
+      close(new_socket);
+    }
+  } catch (system_error_stacktrace& e) {
+    std::cout << "Caught system_error with code " << e.code()
+      << " meaning " << e.what() << '\n' <<e.stacktrace() << '\n';
   }
-  return 0;
+  return EXIT_SUCCESS;
 }
