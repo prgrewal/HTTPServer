607588b09cff97a16c1cfd524857b8cfca1af53f
Roman <roman@localhost.localdomain>
2019-03-20 02:29:26 -0400

    updated

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@
 #include <execinfo.h>
 #include <sstream>
 #include <iterator>
-
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <netdb.h>
+#define MAXEVENTS 64
 std::string process(std::string const& s) {
   std::string::size_type pos = s.find(' ');
   if (pos != std::string::npos) {
@@ -19,6 +22,24 @@ std::string process(std::string const& s) {
   } else {
     return s;
   }
+}
+
+int make_socket_non_blocking(int sfd) {
+  int flags;
+
+  flags = fcntl(sfd, F_GETFL, 0);
+  if (flags == -1) {
+    perror("fcntl");
+    return -1;
+  }
+
+  flags |= O_NONBLOCK;
+  if (fcntl(sfd, F_SETFL, flags) == -1) {
+    perror("fcntl");
+    return -1;
+  }
+
+  return 0;
 }
 
 enum requestMethods {
@@ -43,7 +64,7 @@ class system_error_stacktrace : public std::system_error {
 
     std::ostringstream imploded;
     std::copy(v.begin(), v.end(),
-                   std::ostream_iterator<std::string>(imploded, delim));
+        std::ostream_iterator<std::string>(imploded, delim));
     free(symbols);
     return imploded.str();
   }
@@ -51,7 +72,7 @@ class system_error_stacktrace : public std::system_error {
 
 int main () {
   int server_fd; 
-  int new_socket; 
+  int new_socket;
   long valread;
   struct sockaddr_in address;
   int addrlen = sizeof(address);
@@ -85,63 +106,144 @@ int main () {
       throw system_error_stacktrace(std::error_code(errno, std::generic_category()), "Error in listen");
     }
 
+    int epoll_fd = epoll_create1(0);
+
+    if (epoll_fd < 0) {
+      throw system_error_stacktrace(std::error_code(epoll_fd, std::generic_category()), "Error in epoll_create1");
+    }
+
+    struct epoll_event event, *events;
+
+    event.data.fd = server_fd;
+    event.events = EPOLLIN | EPOLLET;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+      throw system_error_stacktrace(std::error_code(epoll_fd, std::generic_category()), "Error in epoll_ctl");
+      exit(1);
+    }
+
+    events =(epoll_event*)calloc(MAXEVENTS, sizeof(event));
+
     while (true) {
-      if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        throw system_error_stacktrace(std::error_code(new_socket, std::generic_category()), "Error in accept");
+
+      int n, i;
+
+      n = epoll_wait(epoll_fd, events, MAXEVENTS, -1);
+      for (i =0; i< n; i++) {
+        if (events[i].events & EPOLLERR || events[i].events & EPOLLHUP ||
+            !(events[i].events & EPOLLIN)) {
+          /* An error on this fd or socket not ready */
+          throw system_error_stacktrace(std::error_code(epoll_fd, std::generic_category()), "Error in epoll");
+          close(events[i].data.fd);
+        } else if (events[i].data.fd == server_fd) {
+          /* New incoming connection */
+          struct epoll_event event;
+          struct sockaddr in_addr;
+          socklen_t in_len = sizeof(in_addr);
+          char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+
+          //  while ((new_socket = accept(server_fd, &in_addr, &in_len)) != -1) {
+          if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            throw system_error_stacktrace(std::error_code(new_socket, std::generic_category()), "Error in accept");
+          }
+          if (getnameinfo(&in_addr, in_len,
+                hbuf, sizeof(hbuf),
+                sbuf, sizeof(sbuf),
+                NI_NUMERICHOST | NI_NUMERICHOST) == 0) {
+            printf("Accepted connection on descriptor %d (host=%s, port=%s)\n",
+                new_socket, hbuf, sbuf);
+          }
+          /* Make the incoming socket non-block
+           * and add it to list of fds to
+           * monitor*/
+          if (make_socket_non_blocking(new_socket) == -1) {
+            abort();
+          }
+
+          event.data.fd = new_socket;
+          event.events = EPOLLIN | EPOLLET;
+          if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_socket, &event) == -1) {
+            perror("epoll_ctl");
+            abort();
+          }
+          in_len = sizeof(in_addr);
+
+          if (errno != EAGAIN && errno != EWOULDBLOCK)
+            perror("accept");
+          /* else
+           *
+           * We hae processed all incomming connectioins
+           *
+           */
+          /* Data incoming on fd */
+          ssize_t count;
+          std::vector<char> buffer(30000);
+
+          /*while ((*/count = read(new_socket , &buffer[0], buffer.size());/*)) {*/
+          if (count == -1) {
+            /* EAGAIN, read all data */
+            if (errno == EAGAIN)
+              return 1;
+
+            throw system_error_stacktrace(std::error_code(epoll_fd, std::generic_category()), "Error in read");
+            break;
+          }
+
+          std::string str(buffer.begin(), buffer.end());
+          std::cout << str << std::endl;
+
+          std::string method = process(str);
+          std::string result;
+          std::string body;
+          int contentLength;
+          std::string contentLengthStr;
+          switch (requestsMethodsMap.find(method)->second) {
+            case GET: 
+              body = "YOU SENT A GET REQUEST\n";
+              contentLength = body.length();
+              contentLengthStr = std::to_string(contentLength);
+              result = responseHeader + contentLengthStr + "\n\n" + body;
+              break;
+            case POST:
+              body = "YOU SENT A POST REQUEST\n";
+              contentLength = body.length();
+              contentLengthStr = std::to_string(contentLength);
+              result = responseHeader + contentLengthStr + "\n\n" + body;
+              break;
+            case PUT:
+              body = "YOU SENT A PUT REQUEST\n";
+              contentLength = body.length();
+              contentLengthStr = std::to_string(contentLength);
+              result = responseHeader + contentLengthStr + "\n\n" + body;
+              break;
+            case PATCH:
+              body = "YOU SENT A PATCH REQUEST\n";
+              contentLength = body.length();
+              contentLengthStr = std::to_string(contentLength);
+              result = responseHeader + contentLengthStr + "\n\n" + body;
+              break;
+            case DELETE:
+              body = "YOU SENT A DELETE REQUEST\n";
+              contentLength = body.length();
+              contentLengthStr = std::to_string(contentLength);
+              result = responseHeader + contentLengthStr + "\n\n" + body;
+              break;
+            default:
+              body = "METHOD NOT ALLOWED\n";
+              contentLength = body.length();
+              contentLengthStr = std::to_string(contentLength);
+              result = responseHeaderNotAllowed + contentLengthStr + "\n\n" + body;
+              break;
+          }
+
+          write(new_socket, result.c_str(), result.length());
+
+
+          // printf("Close connection on descriptor: %d\n", events[i].data.fd);
+          ////  close(events[i].data.fd);
+          //}
+          //}
+        }
       }
-
-      std::vector<char> buffer(30000);
-      valread = read(new_socket , &buffer[0], buffer.size());
-      std::string str(buffer.begin(), buffer.end());
-      std::cout << str << std::endl;
-
-      std::string method = process(str);
-      std::string result;
-      std::string body;
-      int contentLength;
-      std::string contentLengthStr;
-      switch (requestsMethodsMap.find(method)->second) {
-        case GET: 
-          body = "YOU SENT A GET REQUEST\n";
-          contentLength = body.length();
-          contentLengthStr = std::to_string(contentLength);
-          result = responseHeader + contentLengthStr + "\n\n" + body;
-          break;
-        case POST:
-          body = "YOU SENT A POST REQUEST\n";
-          contentLength = body.length();
-          contentLengthStr = std::to_string(contentLength);
-          result = responseHeader + contentLengthStr + "\n\n" + body;
-          break;
-        case PUT:
-          body = "YOU SENT A PUT REQUEST\n";
-          contentLength = body.length();
-          contentLengthStr = std::to_string(contentLength);
-          result = responseHeader + contentLengthStr + "\n\n" + body;
-          break;
-        case PATCH:
-          body = "YOU SENT A PATCH REQUEST\n";
-          contentLength = body.length();
-          contentLengthStr = std::to_string(contentLength);
-          result = responseHeader + contentLengthStr + "\n\n" + body;
-          break;
-        case DELETE:
-          body = "YOU SENT A DELETE REQUEST\n";
-          contentLength = body.length();
-          contentLengthStr = std::to_string(contentLength);
-          result = responseHeader + contentLengthStr + "\n\n" + body;
-          break;
-        default:
-          body = "METHOD NOT ALLOWED\n";
-          contentLength = body.length();
-          contentLengthStr = std::to_string(contentLength);
-          result = responseHeaderNotAllowed + contentLengthStr + "\n\n" + body;
-          break;
-      }
-
-      write(new_socket, result.c_str(), result.length());
-
-      close(new_socket);
     }
   } catch (system_error_stacktrace& e) {
     std::cout << "Caught system_error with code " << e.code()
